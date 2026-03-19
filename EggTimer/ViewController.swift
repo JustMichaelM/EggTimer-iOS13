9af14369f61448ebac60d3ab469e51b4fbc41dd2
MichaelM <marczak.michal18@gmail.com>
2026-03-19 13:45:58 +0100

    buttons and ifs

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime: Float = 5
+    let mediumTime: Float = 7
+    let hardTime: Float = 12
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Lubie Placki")
     }
-
+    @IBAction func eggHardness(_ sender: UIButton) {
+        let title: String
+        title = sender.currentTitle!
+        print(title)
+        
+        if title == "Soft" {
+            print(softTime)
+        } else if title == "Medium" {
+            print(mediumTime)
+        } else if title == "Hard" {
+            print(hardTime)
+        }
+    }
+    
 }
